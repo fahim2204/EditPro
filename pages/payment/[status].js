a4b37ece618a7f5d9b66830ca7afbf936264330d
fahim2204 <fahimfaisal1998@gmail.com>
2023-03-14 01:06:58 +0600

    Check Loading

--- pages/payment/[status].js
+++ pages/payment/[status].js
@@ -27,40 +27,48 @@ const Home = () => {
       <MainLayout>
         <div className="py-5 px-4">
           <div className="col-lg-8 mx-auto">
-            <div
-              style={{ maxWidth: "500px" }}
-              className="card mx-auto p-4 d-flex justify-content-center align-items-center"
-            >
+            {status ? (
               <div
-                className="p-2 rounded-circle d-flex justify-content-center align-items-center shadow-sm"
-                style={{
-                  height: "150px",
-                  width: "150px",
-                  background: "#F8FAF5",
-                  margin: "0 auto",
-                }}
+                style={{ maxWidth: "500px" }}
+                className="card mx-auto p-4 d-flex justify-content-center align-items-center"
               >
-                <i
-                  className={`${
-                    status === "success" ? "text-success" : "text-danger"
-                  } fw-bolder display-1`}
+                <div
+                  className="p-2 rounded-circle d-flex justify-content-center align-items-center shadow-sm"
+                  style={{
+                    height: "150px",
+                    width: "150px",
+                    background: "#F8FAF5",
+                    margin: "0 auto",
+                  }}
                 >
-                  {status === "success" ? "✓" : "✘"}
-                </i>
+                  <i
+                    className={`${
+                      status === "success" ? "text-success" : "text-danger"
+                    } fw-bolder display-1`}
+                  >
+                    {status === "success" ? "✓" : "✘"}
+                  </i>
+                </div>
+                <h1
+                  className={`mt-2 ${
+                    status === "success" ? "text-success" : "text-danger"
+                  }`}
+                >
+                  {status === "success" ? "SUCCESS" : "FAILED"}
+                </h1>
+                <p>
+                  {status === "success"
+                    ? "Transaction Successful"
+                    : "Your transaction has failed."}
+                </p>
               </div>
-              <h1
-                className={`mt-2 ${
-                  status === "success" ? "text-success" : "text-danger"
-                }`}
-              >
-                {status === "success" ? "SUCCESS" : "FAILED"}
-              </h1>
-              <p>
-                {status === "success"
-                  ? "Transaction Successful"
-                  : "Your transaction has failed."}
-              </p>
-            </div>
+            ) : (
+              <PulseLoader
+                className="text-center my-5"
+                color="green"
+                size={20}
+              />
+            )}
           </div>
         </div>
       </MainLayout>
